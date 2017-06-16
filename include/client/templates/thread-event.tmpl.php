d91f514870766ab4b6013fe1a2b8d353f4451d1e
Joseph Philbert <joe@philbertphotos.com>
2017-06-16 09:04:35 -0400

    Update thread-event.tmpl.php

--- include/client/templates/thread-event.tmpl.php
+++ include/client/templates/thread-event.tmpl.php
@@ -3,13 +3,9 @@ $desc = $event->getDescription(ThreadEvent::MODE_CLIENT);
 if (!$desc)
     return;
 ?>
-<div class="media">
-<div class="media-body">
 <div class="thread-event <?php if ($event->uid) echo 'action'; ?>">
         <span class="type-icon">
           <i class="faded icon-<?php echo $event->getIcon(); ?>"></i>
         </span>
-        <span class="desc"><?php echo $desc; ?></span>
-</div>
-</div>
+        <span class="faded description"><?php echo $desc; ?></span>
 </div>
